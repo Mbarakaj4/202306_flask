f774d2cdb02ec7c798977538f8cfc5a227d75298
Mbarakaj4 <113691935+Mbarakaj4@users.noreply.github.com>
2023-08-05 16:50:12 -0400

    Update index.html and server.py
    Added jinja template and arguments into the render template on server.

--- Practice/CodingDojo-HelloFlask/server.py
+++ Practice/CodingDojo-HelloFlask/server.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hola_flask ():
-    return 'Hola Flask!'
+    return render_template('index.html', frase='Hola', times=5)
 
 @app.route('/success')
 def success():
